ea053d83ee66d601597099a6bfb486bd4068abf9
krog-13 <virtualspook@gmail.com>
2022-02-10 14:03:05 +0600

    sqlalchemy

--- config.py
+++ config.py
@@ -5,10 +5,12 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DOSSIER_PER_PAGE = 4
     UPLOAD_FOLDER = os.path.join(basedir, 'app/templates/static')
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'jpeg', 'jpg'}
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
