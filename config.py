a9f601ca6678349c4204d95597af6be000d0fbbd
krog-13 <virtualspook@gmail.com>
2022-02-10 14:45:46 +0600

    delete alembic

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-class Config:
+class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
